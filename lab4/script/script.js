c9a204581d152090e396d517d8d5484780e64399
Aleksandr Klachkevich <Aleksandr Klachkevich>
2016-11-23 16:54:51 +0300

    lab4

--- lab4/script/script.js
+++ lab4/script/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let arr = [];
-let page = 0;
+let page = 1;
 
 $(document).ready(function () {
 
@@ -55,14 +55,22 @@ $(document).ready(function () {
     $('.tile').css("background-image","url('img/background10.jpg')");
   });
 
+  $('.close').click(function() {
+    $('.m-window').css("display","none");
+    $('#wrap').css("display","none");
+  })
+
+  $('#wrap').click(function () {
+    $('.m-window').css("display","none");
+    $('#wrap').css("display","none");
+    $('.load').css("display","none");
+  });
+
   $('.city').keydown(function(event) {
     if(event.keyCode == 13){
       page = 0;
       showMore();
-      page++;
-      $('html, body').animate({
-        scrollTop:$('.items').offset().top
-      },1000);
+
     }
   });
 
@@ -81,21 +89,27 @@ $(document).ready(function () {
         'listing_type': 'buy',
         'place_name': city
       },
+      beforeSend: function () {
+        $('#wrap').css("display","block");
+        $('.load').css("display","block");
+      },
       success: function (data) {
         arr = data.response.listings;
 
         let tmpl = '\
           <% for (var i=0; i < count.length; i++) { %> \
-            <div class="listing-list" >\
-            <img class="" src="<%=count[i].img_url%>"> \
-            <span class="price"><%=count[i].price_formatted%></span><br>\
-            <span class="title"><%=count[i].title%></span><br>\
-            <span class="summary"><%=count[i].summary%></span><br>\
-            <span class="updated"><%=count[i].updated_in_days_formatted%></span>\
+            <div class="listing-list" onClick="moreInfo(<%=i%>)" >\
+              <img class="" src="<%=count[i].img_url%>"> \
+              <span class="price"><%=count[i].price_formatted%></span><br>\
+              <span class="title"><%=count[i].title%></span><br>\
+              <span class="summary"><%=count[i].summary%></span><br>\
+              <span class="updated"><%=count[i].updated_in_days_formatted%></span>\
             </div>\
           <% } %>\
           <span class="show-more" onClick="showMore()">Show more</span>';
         $('.items').html( _.template(tmpl)({count: arr}) );
+        $('#wrap').css("display","none");
+        $('.load').css("display","none");
         $('html, body').animate({
           scrollTop:$('.items').offset().top
         },1000);
@@ -126,25 +140,41 @@ function showMore() {
       'page': ++page,
       'place_name': city
     },
+    beforeSend: function () {
+      $('#wrap').css("display","block");
+      $('.load').css("display","block");
+    },
     success: function (data) {
       a = data.response.listings;
       $('.show-more').remove();
       let tmpl = '\
         <% for (var i=0; i < count.length; i++) { %> \
-          <div class="listing-list">\
-          <img class="" src="<%=count[i].img_url%>"> \
-          <span class="price"><%=count[i].price_formatted%></span><br>\
-          <span class="title"><%=count[i].title%></span><br>\
-          <span class="summary"><%=count[i].summary%></span><br>\
-          <span class="updated"><%=count[i].updated_in_days_formatted%></span>\
+          <div class="listing-list" onClick="moreInfo(<%=i%>)" >\
+            <img class="" src="<%=count[i].img_url%>"> \
+            <span class="price"><%=count[i].price_formatted%></span><br>\
+            <span class="title"><%=count[i].title%></span><br>\
+            <span class="summary"><%=count[i].summary%></span><br>\
+            <span class="updated"><%=count[i].updated_in_days_formatted%></span>\
           </div>\
         <% } %>\
         <span class="show-more" onClick="showMore()">Show more</span>';
       $('.items').append( _.template(tmpl)({count: a}) );
+      $('#wrap').css("display","none");
+      $('.load').css("display","none");
+      if(page == 1){
+        $('html, body').animate({
+          scrollTop:$('.items').offset().top
+        },1000);
+      }
       arr = _.union(arr, a);
     },
     error: function () {
       alert('not ok');
     }
   });
+}
+
+function moreInfo(i) {
+  $('.m-window').css("display","block");
+  $('#wrap').css("display","block");
 }
