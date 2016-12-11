902446cb185b65fc3423b8a34d83b97eb9c8f8d1
Aleksandr Klachkevich <Aleksandr Klachkevich>
2016-12-11 14:58:38 +0300

    v0.2

--- lab5/script/script.js
+++ lab5/script/script.js
@@ -2,19 +2,256 @@
 
 let app = angular.module('app', ['ngRoute']);
 app.config( ['$routeProvider', function($routeProvider) {
+
 		$routeProvider
-			.when('/first', {
-				templateUrl: 'tamplate.html'
+			.when('/0', {
+				templateUrl: 'tamplate.html',
+				controller: 'zero'
 			})
-			.when('/second', {
-				templateUrl: 'tamplate.html'
+      .when('/1', {
+				templateUrl: 'tamplate.html',
+				controller: 'first'
+			})
+      .when('/2', {
+				templateUrl: 'tamplate.html',
+				controller: 'second'
+			})
+      .when('/3', {
+				templateUrl: 'tamplate.html',
+				controller: 'third'
+			})
+      .when('/4', {
+				templateUrl: 'tamplate.html',
+				controller: 'fourth'
+			})
+      .when('/5', {
+				templateUrl: 'tamplate.html',
+				controller: 'fifth'
+			})
+      .when('/6', {
+				templateUrl: 'tamplate.html',
+				controller: 'sixth'
+			})
+      .when('/7', {
+				templateUrl: 'tamplate.html',
+				controller: 'seventh'
+			})
+      .when('/8', {
+				templateUrl: 'tamplate.html',
+				controller: 'eighth'
+			})
+      .when('/9', {
+				templateUrl: 'tamplate.html',
+				controller: 'ninth'
+			})
+      .when('/10', {
+				templateUrl: 'tamplate.html',
+				controller: 'tenth'
+			})
+      .when('/11', {
+				templateUrl: 'tamplate.html',
+				controller: 'eleventh'
+			})
+      .when('/12', {
+				templateUrl: 'tamplate.html',
+				controller: 'twelfth'
+			})
+      .when('/13', {
+				templateUrl: 'tamplate.html',
+				controller: 'thirteenth'
+			})
+      .when('/14', {
+				templateUrl: 'tamplate.html',
+				controller: 'fourteenth'
+			})
+      .when('/15', {
+				templateUrl: 'tamplate.html',
+				controller: 'fifteenth'
+			})
+      .when('/16', {
+				templateUrl: 'tamplate.html',
+				controller: 'sixteenth'
+			})
+      .when('/17', {
+				templateUrl: 'tamplate.html',
+				controller: 'seventeenth'
+			})
+      .when('/18', {
+				templateUrl: 'tamplate.html',
+				controller: 'eighteenth'
+			})
+      .when('/19', {
+				templateUrl: 'tamplate.html',
+				controller: 'nineteenth'
 			})
 			.otherwise({
 				redirectTo: '/'
 			});
 	}]);
 
+	app.controller('zero', function ($scope) {
+		$scope.indx = 0;
+		let map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+	    zoom: 17
+	  });
+	});
+
+app.controller('first', function ($scope) {
+	$scope.indx = 1;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('second', function ($scope) {
+	$scope.indx = 2;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('third', function ($scope) {
+	$scope.indx = 3;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('fourth', function ($scope) {
+	$scope.indx = 4;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('fifth', function ($scope) {
+	$scope.indx = 5;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('sixth', function ($scope) {
+	$scope.indx = 6;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('seventh', function ($scope) {
+	$scope.indx = 7;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('eighth', function ($scope) {
+	$scope.indx = 8;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('ninth', function ($scope) {
+	$scope.indx = 9;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('tenth', function ($scope) {
+	$scope.indx = 10;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('eleventh', function ($scope) {
+	$scope.indx = 11;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('twelfth', function ($scope) {
+	$scope.indx = 12;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('thirteenth', function ($scope) {
+	$scope.indx = 13;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('fourteenth', function ($scope) {
+	$scope.indx = 14;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('fifteenth', function ($scope) {
+	$scope.indx = 15;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('sixteenth', function ($scope) {
+	$scope.indx = 16;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('seventeenth', function ($scope) {
+	$scope.indx = 17;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('eighteenth', function ($scope) {
+	$scope.indx = 18;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
+app.controller('nineteenth', function ($scope) {
+	$scope.indx = 19;
+	let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+    zoom: 17
+  });
+});
+
 app.controller('appController', function($scope, $http) {
+
+    $scope.indx = 0;
 
     $scope.itemsPerPage = 1;
 
@@ -31,24 +268,6 @@ app.controller('appController', function($scope, $http) {
       $('.item').html("");
       let url = "http://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=" +  + "&listing_type=" + $('input:checked').val() + "&place_name="+ $('.city').val() +"&price_min=" + price_min + "&price_max=" + price_max + "&bedroom_min=" + bedroom_min + "&bedroom_max=" + bedroom_max +"&room_min=" + room_min;
       url += "&bathroom_min=" + bathroom_min + "&bathroom_max=" + bathroom_max;
-
-      //var params = JSON.stringify({encoding: "json", pretty: "1", action: "search_listings", country: $('.country').val(), listing_type: $('input:checked').val(), place_name: $('.city').val()});
-
-      /*$http.jsonp("http://api.nestoria.co.uk/api/?data=" + params, {jsonpCallbackParam: 'callback'})
-        .then(function(json) {
-            $scope.data = json.data.response.listings;
-        });*/
-
-      /*$http({
-        method : "GET",
-        url : url
-
-      }).then(function (response) {
-          $scope.data = response.data.response.listings;
-          $scope.itemsPerPage = response.data.response.total_pages;
-        }, function (response) {
-          $alert(response.statusText);
-      });*/
 
       $.ajax({
         type: "GET",
@@ -76,12 +295,17 @@ app.controller('appController', function($scope, $http) {
         success: function (data) {
           $scope.data = data.response.listings;
           $scope.itemsPerPage = data.response.total_pages;
+
         },
 
         error: function () {
           alert('not ok');
         }
       });
+			let map = new google.maps.Map(document.getElementById('map'), {
+		    center: {lat: $scope.data[$scope.indx].latitude, lng:  $scope.data[$scope.indx].longitude},
+		    zoom: 17
+		  });
     };
 
 
@@ -187,12 +411,6 @@ app.controller('appController', function($scope, $http) {
       let url = "http://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=" + $('.country').val() + "&listing_type=" + $('input:checked').val() + "&place_name="+ $('.city').val() +"&price_min=" + price_min + "&price_max=" + price_max + "&bedroom_min=" + bedroom_min + "&bedroom_max=" + bedroom_max +"&room_min=" + room_min;
       url += "&bathroom_min=" + bathroom_min + "&bathroom_max=" + bathroom_max + "&page=" + $scope.currentPage;
 
-      /*$http({
-        method : "GET",
-        url : url
-      }).then(function(response) {
-          $scope.data = response.data.response.listings;
-      });*/
       $.ajax({
         type: "GET",
         url: "http://api.nestoria.co.uk/api",
